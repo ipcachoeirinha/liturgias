064a360f205148fafca1077cdc9de63b35c8ff19
vv-z84 <43834700+vv-z84@users.noreply.github.com>
2023-09-13 10:18:52 -0300

    Add liturgia do dia 17 de setembro 2023

---
+++ ipca-site/app/routes/liturgias.091723-desfrutando-da-verdadeira-liberdade-em-cristo.download.jsx
@@ -0,0 +1,16 @@
+import defaultConfig from '../config/revealjs-default.json'
+import { downloadSlideRemixLoader } from '../lib.server'
+
+const PAGE_TITLE = "Desfrutando da Verdadeira Liberdade em Cristo - 17 de Setembro de 2023"
+const CDN_ASSETS_LIST = [
+  { id: "ipcachoeirinha/cruz-wallpaper-light", extension: "jpg", resourceType: "image" },
+  { id: "ipcachoeirinha/hino-386", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/hino-110", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/hino-86", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/hino-135", extension: "mp4", resourceType: "video" },
+]
+const ZIP_FILENAME = "091723-desfrutando-da-verdadeira-liberdade-em-cristo"
+
+export async function loader(remixLoaderContext) {
+  return downloadSlideRemixLoader(remixLoaderContext, ZIP_FILENAME, CDN_ASSETS_LIST, PAGE_TITLE, defaultConfig)
+}
