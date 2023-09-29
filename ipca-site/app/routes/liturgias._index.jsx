0162ca02a02c24f30426e1b07116b28e0fd133be
vv-z84 <43834700+vv-z84@users.noreply.github.com>
2023-09-29 11:06:22 -0300

    Add liturgy 01 out 2023

--- ipca-site/app/routes/liturgias._index.jsx
+++ ipca-site/app/routes/liturgias._index.jsx
@@ -6,6 +6,7 @@ export default function Liturgias()
       <Link className="" to="/liturgias/090323-a-transformacao-pela-graca">A Transformação pela Graça - 03 de Setembro de 2023</Link><br />
       <Link className="" to="/liturgias/091023-os-filhos-e-herdeiros-em-cristo">Os Filhos e Herdeiros em Cristo - 10 de Setembro de 2023</Link><br />
       <Link className="" to="/liturgias/091723-desfrutando-da-verdadeira-liberdade-em-cristo">Desfrutando da Verdadeira Liberdade em Cristo - 17 de Setembro de 2023</Link><br />
-      <Link className="" to="/liturgias/092423-uma-igreja-nascida-do-espirito">Uma Igreja Nascida do Espírito - 24 de Setembro de 2023</Link>
+      <Link className="" to="/liturgias/092423-uma-igreja-nascida-do-espirito">Uma Igreja Nascida do Espírito - 24 de Setembro de 2023</Link><br />
+      <Link className="" to="/liturgias/100123-compromissados-com-a-liberdade-em-cristo">Compromissados com a Liberdade em Cristo - 01 de Outubro de 2023</Link><br />
     </div>)
 }
