47cff6e8404ca80e3caae08f46179d56f550d188
vv-z84 <43834700+vv-z84@users.noreply.github.com>
2023-10-22 12:12:47 -0300

    Add link to liturgy of oct 22 23

--- ipca-site/app/routes/liturgias._index.jsx
+++ ipca-site/app/routes/liturgias._index.jsx
@@ -1,14 +1,14 @@
 import { Link } from '@remix-run/react'
 
-export default function Liturgias()
-{
-    return (<div>
-      <Link className="" to="/liturgias/090323-a-transformacao-pela-graca">A Transformação pela Graça - 03 de Setembro de 2023</Link><br />
-      <Link className="" to="/liturgias/091023-os-filhos-e-herdeiros-em-cristo">Os Filhos e Herdeiros em Cristo - 10 de Setembro de 2023</Link><br />
-      <Link className="" to="/liturgias/091723-desfrutando-da-verdadeira-liberdade-em-cristo">Desfrutando da Verdadeira Liberdade em Cristo - 17 de Setembro de 2023</Link><br />
-      <Link className="" to="/liturgias/092423-uma-igreja-nascida-do-espirito">Uma Igreja Nascida do Espírito - 24 de Setembro de 2023</Link><br />
-      <Link className="" to="/liturgias/100123-compromissados-com-a-liberdade-em-cristo">Compromissados com a Liberdade em Cristo - 01 de Outubro de 2023</Link><br />
-      <Link className="" to="/liturgias/100823-uma-igreja-que-reconhece-sua-miseria-espiritual">Uma Igreja que Reconhece Sua Miséria Espiritual - 08 de Outubro de 2023</Link><br />
-      <Link className="" to="/liturgias/101523-aprendendo-a-esperar-em-deus">Aprendendo a Esperar em Deus - 15 de Outubro de 2023</Link><br />
-    </div>)
+export default function Liturgias() {
+  return (<div>
+    <Link className="" to="/liturgias/090323-a-transformacao-pela-graca">A Transformação pela Graça - 03 de Setembro de 2023</Link><br />
+    <Link className="" to="/liturgias/091023-os-filhos-e-herdeiros-em-cristo">Os Filhos e Herdeiros em Cristo - 10 de Setembro de 2023</Link><br />
+    <Link className="" to="/liturgias/091723-desfrutando-da-verdadeira-liberdade-em-cristo">Desfrutando da Verdadeira Liberdade em Cristo - 17 de Setembro de 2023</Link><br />
+    <Link className="" to="/liturgias/092423-uma-igreja-nascida-do-espirito">Uma Igreja Nascida do Espírito - 24 de Setembro de 2023</Link><br />
+    <Link className="" to="/liturgias/100123-compromissados-com-a-liberdade-em-cristo">Compromissados com a Liberdade em Cristo - 01 de Outubro de 2023</Link><br />
+    <Link className="" to="/liturgias/100823-uma-igreja-que-reconhece-sua-miseria-espiritual">Uma Igreja que Reconhece Sua Miséria Espiritual - 08 de Outubro de 2023</Link><br />
+    <Link className="" to="/liturgias/101523-aprendendo-a-esperar-em-deus">Aprendendo a Esperar em Deus - 15 de Outubro de 2023</Link><br />
+    <Link className="" to="/liturgias/102223-uma-igreja-que-anda-no-poder-do-espirito">Uma Igreja que Anda no Poder do Espírito - 22 de Outubro de 2023</Link><br />
+  </div>)
 }
