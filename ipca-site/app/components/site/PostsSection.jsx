a8be976d685579d602311c7d02480a26efc77309
vv-z84 <43834700+vv-z84@users.noreply.github.com>
2023-11-11 09:04:16 -0300

    staging

--- ipca-site/app/components/site/PostsSection.jsx
+++ ipca-site/app/components/site/PostsSection.jsx
@@ -4,7 +4,7 @@ export function PostsSection({ posts }) {
     const postsElements = posts.map(p => <Post key={p.url} post={p} />)
 
     return (<section className='px-4'>
-        <h1 className='text-left text-yellow-600 text-3xl font-bold mb-4'>Pastorais</h1>
+        <h1 className='text-yellow-600 text-3xl font-bold mb-4 text-center md:text-left'>Pastorais</h1>
         <div className='flex flex-col md:flex-row md:space-x-2'>
             {postsElements}
         </div>
@@ -15,7 +15,7 @@ function Post({ post }) {
     const date = new Date(post.createdAt)
     const formatted = `${date.getDate()}/${date.getMonth()}`
 
-    return (<div style={{ background: `url('/${post.cover}')` }} className='p-4 basis-1/4 mb-2 md:mb-0'>
+    return (<div className='bg-pink-300 p-4 basis-1/4 mb-2 md:mb-0'>
         <p>{formatted}</p>
         <h2>{post.title}</h2>
         <p>{post.description}</p>
