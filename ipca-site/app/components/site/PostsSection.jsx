eaca95008261f061bf51b00a93da6e6ccad31a15
vv-z84 <43834700+vv-z84@users.noreply.github.com>
2023-11-16 16:03:48 -0300

    Initial implementation of the website

--- ipca-site/app/components/site/PostsSection.jsx
+++ ipca-site/app/components/site/PostsSection.jsx
@@ -3,7 +3,7 @@ import { Link } from '@remix-run/react'
 export function PostsSection({ posts }) {
     const postsElements = posts.map(p => <Post key={p.url} post={p} />)
 
-    return (<section className='px-4'>
+    return (<section className='p-4'>
         <h1 className='text-yellow-600 text-3xl font-bold mb-4 text-center md:text-left'>Pastorais</h1>
         <div className='flex flex-col md:flex-row md:space-x-2'>
             {postsElements}
@@ -15,10 +15,10 @@ function Post({ post }) {
     const date = new Date(post.createdAt)
     const formatted = `${date.getDate()}/${date.getMonth()}`
 
-    return (<div className='bg-pink-300 p-4 basis-1/4 mb-2 md:mb-0'>
+    return (<div className='p-4 basis-1/4 mb-2 md:mb-0'>
         <p>{formatted}</p>
         <h2>{post.title}</h2>
         <p>{post.description}</p>
-        <Link to={`/pastoral${post.url}`}>Ler</Link>
+        <Link className='text-blue-500 underline' to={`/pastoral${post.url}`}>Ler</Link>
     </div>)
 }
--- ipca-site/app/routes/_index.jsx
+++ ipca-site/app/routes/_index.jsx
@@ -13,52 +13,52 @@ export const meta = () => {
  */
 export function loader() {
     const data = [
-        // {
-        //     id: 1,
-        //     url: "/pastoral-teste",
-        //     title: "Pastoral Teste",
-        //     description: "Uma pastoral de teste",
-        //     cover: "cover.png",
-        //     createdAt: new Date(),
-        //     updatedAt: null
-        // },
-        // {
-        //     id: 2,
-        //     url: "/pastoral-teste-2",
-        //     title: "Pastoral Teste 2",
-        //     description: "Uma pastoral de teste 2",
-        //     cover: "cover.png",
-        //     createdAt: new Date(),
-        //     updatedAt: null
-        // },
-        // {
-        //     id: 1,
-        //     url: "/pastoral-teste",
-        //     title: "Pastoral Teste",
-        //     description: "Uma pastoral de teste",
-        //     cover: "cover.png",
-        //     createdAt: new Date(),
-        //     updatedAt: null
-        // },
-        // {
-        //     id: 2,
-        //     url: "/pastoral-teste-2",
-        //     title: "Pastoral Teste 2",
-        //     description: "Uma pastoral de teste 2",
-        //     cover: "cover.png",
-        //     createdAt: new Date(),
-        //     updatedAt: null
-        // }
+        {
+            id: 1,
+            url: "/sem-titulo",
+            title: "Sem Título",
+            description: "Sem descrição",
+            cover: "cover.png",
+            createdAt: '2023-11-12 00:00:00',
+            updatedAt: null
+        },
+        {
+            id: 1,
+            url: "/sem-titulo",
+            title: "Sem Título",
+            description: "Sem descrição",
+            cover: "cover.png",
+            createdAt: '2023-11-12 00:00:00',
+            updatedAt: null
+        },
+        {
+            id: 1,
+            url: "/sem-titulo",
+            title: "Sem Título",
+            description: "Sem descrição",
+            cover: "cover.png",
+            createdAt: '2023-11-12 00:00:00',
+            updatedAt: null
+        },
+        {
+            id: 1,
+            url: "/sem-titulo",
+            title: "Sem Título",
+            description: "Sem descrição",
+            cover: "cover.png",
+            createdAt: '2023-11-12 00:00:00',
+            updatedAt: null
+        }
     ]
 
-    return data;
+    return data
 }
 
 export default function Index() {
     const postsData = useLoaderData()
 
     return (<>
-        <header className='bg-slate-700 py-4 px-8'>
+        <header className='bg-slate-700 py-4 px-8 mb-4'>
             <div className='max-w-5xl m-auto flex items-center space-x-4 flex-col md:flex-row'>
                 <div className='flex items-center space-x-4 basis-2/5 mb-4 md:mb-0'>
                     <div>
@@ -79,9 +79,9 @@ export default function Index() {
             </div>
         </header>
         <main>
-            <div className='bg-[url("/bg5.svg")] bg-cover px-8 py-4 min-h-[500px] flex items-center pb-[100px]'>
+            <div className='px-8 py-4 min-h-[500px] flex items-center pb-[100px]'>
                 <div className='flex flex-col md:flex-row max-w-5xl m-auto justify-around items-center space-x-4'>
-                    <section className='basis-1/4 grow-0 mb-4 md:mb-0'>
+                    <section className='basis-1/4 grow-0 mb-6 md:mb-0'>
                         <h1 className='text-center text-yellow-600 text-3xl font-bold mb-4'>Programações</h1>
                         <p className='text-green-950 text-lg text-center font-bold'>Culto Solene</p>
                         <p className='text-green-950 text-lg text-center mb-4'>Domingo, 18:00</p>
@@ -95,7 +95,45 @@ export default function Index() {
                     </div>
                 </div>
             </div>
-            <div className='max-w-5xl m-auto'>
+
+            <div className='max-w-5xl m-auto mb-6'>
+                <section className='px-4'>
+                    <h2 className='text-center md:text-left text-green-950 text-xl font-bold'>CONHEÇA A NOSSA IGREJA</h2>
+                    <h1 className='text-center md:text-left text-yellow-600 text-3xl font-bold mb-4'>IGREJA PRESBITERIANA DE CACHOEIRINHA</h1>
+                    <p className='mb-4 text-lg text-justify text-green-950'>Uma igreja que por meio de Cristo, honra a Deus com a plenitude da vida, alegrando-se Nele plenamente!</p>
+
+                    <div className='flex justify-center'>
+                        <Link
+                            className='mb-4 bg-blue-400 hover:bg-blue-600 underline text-center text-white font-bold py-4 px-6 rounded-md'
+                            to={"/sobre-a-igreja"}>Sobre a Igreja</Link>
+                    </div>
+                </section>
+            </div>
+
+            <div className='bg-slate-700 py-6'>
+                <div className='max-w-5xl m-auto mb-6'>
+                    <section className='px-4'>
+                        <h1 className='text-center text-yellow-600 text-3xl font-bold mb-4'>Contribuições</h1>
+                        <p className='mb-4 text-lg text-justify text-slate-100'>Contamos com sua ajuda e você pode contribuir utilizando os dados abaixo para dízimo e ofertas.</p>
+                        <div className='flex mb-4'>
+                            <div>
+                            </div>
+                            <div>
+                                <ul className='text-slate-100'>
+                                    <li><span className='font-bold'>IGREJA PRESBITERIANA DO BRASIL EM CANOAS</span></li>
+                                    <li><span className='font-bold'>Bradesco</span></li>
+                                    <li><span className='font-bold'>Agência</span> 0000</li>
+                                    <li><span className='font-bold'>Conta</span> 0000000-0</li>
+                                    <li><span className='font-bold'>CNPJ</span> 0000000-0</li>
+                                    <li><span className='font-bold'>Chave PIX</span> email@email.com</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <p className='bg-yellow-100 text-yellow-800 p-4 text-sm text-justify border-solid border border-yellow-300 rounded'>Atenção ao copiar os dados e informações para dízimo e ofertas, não nos responsabilizamos por contribuições realizadas com informações divergentes das apresentadas em nosso site.</p>
+                    </section>
+                </div>
+            </div>
+            <div className='max-w-5xl m-auto mb-6'>
                 <PostsSection posts={postsData} />
             </div>
         </main>
