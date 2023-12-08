095e41f7930109ef3529d74c8d90fa5799659cb4
vv-z84 <43834700+vv-z84@users.noreply.github.com>
2023-12-08 10:14:57 -0300

    Add slides for Dec 10

---
+++ ipca-site/app/routes/liturgias.121023-a-encarnacao-de-jesus-cristo.jsx
@@ -0,0 +1,176 @@
+import { useLoaderData } from '@remix-run/react'
+import { useEffect } from 'react'
+import defaultConfig from '../config/revealjs-default.json'
+import { getAssetUrlBuilder } from '../lib'
+import { presentationSlideRemixLoader } from '../lib.server'
+import { createSlideSectionWithBackgroundImage } from '../components/ipca-default-theme/DefaultSlideSection'
+import { RevealJsBaseElement } from '../components/RevealJsBaseElement'
+import { MainTitle } from '../components/ipca-default-theme/MainTitle'
+import { SubTitle } from '../components/ipca-default-theme/SubTitle'
+import { LiturgySteps } from '../components/ipca-default-theme/LiturgyStepsList'
+import { LITURGY_STEP_TYPES, LiturgyStep } from '../components/ipca-default-theme/LiturgyStep'
+import { VideoSlide } from '../components/ipca-default-theme/VideoSlide'
+
+const PAGE_TITLE = "A Encarnação de Jesus Cristo - 10 de Dezembro de 2023"
+const CDN_ASSETS_LIST = [
+  { id: "ipcachoeirinha/cruz-wallpaper-light", extension: "jpg", resourceType: "image" },
+  { id: "ipcachoeirinha/hino-237", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/hino-93", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/hino-54", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/hino-239", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/hino-001-instrumental", extension: "mp3", resourceType: "video" },
+  { id: "ipcachoeirinha/uma-bencao-antiga-letra", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/what-a-beautiful-name-hillsong-cover-daniel-jang", extension: "mp3", resourceType: "video" },
+]
+
+export const meta = () => {
+  return [
+    { title: PAGE_TITLE }
+  ]
+}
+
+export async function loader(remixLoaderContext) {
+  return presentationSlideRemixLoader(remixLoaderContext, CDN_ASSETS_LIST)
+}
+
+export default function AEncarnacaoDeJesusCristo() {
+  const [mode, assets] = useLoaderData()
+
+  useEffect(() => {
+    window.Reveal = require('reveal.js').default
+
+    if (window.Reveal) {
+      const r = window.Reveal()
+      r.initialize({ ...defaultConfig, width: 1600 })
+    }
+  })
+
+  const getAssetUrl = getAssetUrlBuilder(mode, assets)
+  const SlideWithBackground = createSlideSectionWithBackgroundImage(getAssetUrl("ipcachoeirinha/cruz-wallpaper-light"))
+
+  return (
+    <RevealJsBaseElement>
+      <SlideWithBackground>
+        <MainTitle>A ENCARNAÇÃO DE JESUS CRISTO</MainTitle>
+      </SlideWithBackground>
+
+      <SlideWithBackground>
+        <MainTitle>PRELÚDIO</MainTitle>
+        <audio data-autoplay><source
+          type="audio/mpeg"
+
+          src={getAssetUrl("ipcachoeirinha/hino-001-instrumental")}
+
+
+        />O seu navegador não suporta o elemento <code>audio</code>.</audio>
+      </SlideWithBackground>
+
+      <SlideWithBackground>
+        <section>
+          <MainTitle>A ENCARNAÇÃO DE JESUS CRISTO</MainTitle>
+          <SubTitle>É A RAZÃO DE NOSSA ADORAÇÃO</SubTitle>
+
+          <LiturgySteps>
+            <LiturgyStep liturgyStepType={LITURGY_STEP_TYPES.reading}>: SALMO 132</LiturgyStep>
+            <LiturgyStep liturgyStepType={LITURGY_STEP_TYPES.worship_prayer}></LiturgyStep>
+            <LiturgyStep liturgyStepType={LITURGY_STEP_TYPES.hymn}>: JESUS NASCEU, nº 237</LiturgyStep>
+          </LiturgySteps>
+        </section>
+        <VideoSlide videoUrl={getAssetUrl("ipcachoeirinha/hino-237")} />
+      </SlideWithBackground>
+
+      <SlideWithBackground>
+        <section>
+          <MainTitle>A ENCARNAÇÃO DE JESUS CRISTO</MainTitle>
+          <SubTitle>TROUXE REDENÇÃO PARA O SEU POVO</SubTitle>
+
+          <LiturgySteps>
+            <LiturgyStep liturgyStepType={LITURGY_STEP_TYPES.reading}>: ISAÍAS 53:10-12</LiturgyStep>
+            <LiturgyStep liturgyStepType={LITURGY_STEP_TYPES.silent_prayer}></LiturgyStep>
+            <LiturgyStep liturgyStepType={LITURGY_STEP_TYPES.repent_prayer}></LiturgyStep>
+            <LiturgyStep liturgyStepType={LITURGY_STEP_TYPES.hymn}>: FIRMEZA NA FÉ, nº 93</LiturgyStep>
+          </LiturgySteps>
+        </section>
+        <section>
+
+          <MainTitle>ORAÇÃO SILENCIOSA</MainTitle>
+
+          <audio data-autoplay><source
+            type="audio/mpeg"
+
+            src={getAssetUrl("ipcachoeirinha/what-a-beautiful-name-hillsong-cover-daniel-jang")}
+
+
+          />O seu navegador não suporta o elemento <code>audio</code>.</audio>
+        </section>
+
+        <section>
+          <MainTitle>A ENCARNAÇÃO DE JESUS CRISTO</MainTitle>
+          <SubTitle>TROUXE REDENÇÃO PARA O SEU POVO</SubTitle>
+
+          <LiturgySteps>
+            <LiturgyStep liturgyStepType={LITURGY_STEP_TYPES.reading}>: ISAÍAS 53:10-12</LiturgyStep>
+            <LiturgyStep liturgyStepType={LITURGY_STEP_TYPES.silent_prayer}></LiturgyStep>
+            <LiturgyStep liturgyStepType={LITURGY_STEP_TYPES.repent_prayer}></LiturgyStep>
+            <LiturgyStep liturgyStepType={LITURGY_STEP_TYPES.hymn}>: FIRMEZA NA FÉ, nº 93</LiturgyStep>
+          </LiturgySteps>
+        </section>
+
+        <VideoSlide videoUrl={getAssetUrl("ipcachoeirinha/hino-93")} />
+      </SlideWithBackground>
+
+      <SlideWithBackground>
+        <section>
+          <MainTitle>A ENCARNAÇÃO DE JESUS CRISTO</MainTitle>
+          <SubTitle>FOI ANUNCIADA PELOS PROFETAS</SubTitle>
+
+          <LiturgySteps>
+            <LiturgyStep liturgyStepType={LITURGY_STEP_TYPES.unison_reading}>: ISAÍAS 40:1-5</LiturgyStep>
+            <LiturgyStep liturgyStepType={LITURGY_STEP_TYPES.thankful_prayer}></LiturgyStep>
+            <LiturgyStep liturgyStepType={LITURGY_STEP_TYPES.offering}></LiturgyStep>
+            <LiturgyStep liturgyStepType={LITURGY_STEP_TYPES.hymn}>: A CHEGADA DO MESSIAS, nº 54</LiturgyStep>
+          </LiturgySteps>
+        </section>
+        <VideoSlide videoUrl={getAssetUrl("ipcachoeirinha/hino-54")} />
+      </SlideWithBackground>
+
+      <SlideWithBackground>
+        <MainTitle>A ENCARNAÇÃO DE JESUS CRISTO</MainTitle>
+        <SubTitle>REVELA A GLÓRIA DO PAI</SubTitle>
+
+        <LiturgySteps>
+          <LiturgyStep liturgyStepType={LITURGY_STEP_TYPES.pray_for_children}></LiturgyStep>
+          <LiturgyStep liturgyStepType={LITURGY_STEP_TYPES.preaching}>: JOÃO 1:6-14 (Rev. Arnaldo Matias)</LiturgyStep>
+        </LiturgySteps>
+      </SlideWithBackground>
+
+      <SlideWithBackground>
+        <section>
+          <MainTitle>A ENCARNAÇÃO DE JESUS CRISTO</MainTitle>
+          <SubTitle>É ANUNCIADA PELO SEU POVO</SubTitle>
+
+          <LiturgySteps>
+            <LiturgyStep liturgyStepType={LITURGY_STEP_TYPES.hymn}>: NASCE JESUS, nº 239</LiturgyStep>
+            <LiturgyStep liturgyStepType={LITURGY_STEP_TYPES.ending_prayer}></LiturgyStep>
+            <LiturgyStep liturgyStepType={LITURGY_STEP_TYPES.blessings}></LiturgyStep>
+          </LiturgySteps>
+        </section>
+        <VideoSlide videoUrl={getAssetUrl("ipcachoeirinha/hino-239")} />
+      </SlideWithBackground>
+
+      <SlideWithBackground>
+        <section>
+          <MainTitle>POSLÚDIO</MainTitle>
+        </section>
+        <VideoSlide videoUrl={getAssetUrl("ipcachoeirinha/uma-bencao-antiga-letra")} />
+      </SlideWithBackground>
+
+      <SlideWithBackground>
+        <section>
+          <MainTitle>AVISOS</MainTitle>
+        </section>
+      </SlideWithBackground>
+
+    </RevealJsBaseElement>
+  )
+}
