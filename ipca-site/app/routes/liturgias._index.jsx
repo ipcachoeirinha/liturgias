5b48949f35d151b6026c2e4bab9b60dee8e0084b
vv-z84 <43834700+vv-z84@users.noreply.github.com>
2024-01-06 19:45:47 -0300

    Add liturgy for Jan 07 24

--- ipca-site/app/routes/liturgias._index.jsx
+++ ipca-site/app/routes/liturgias._index.jsx
@@ -2,21 +2,6 @@ import { Link } from '@remix-run/react'
 
 export default function Liturgias() {
   return (<div>
-    <Link className="" to="/liturgias/090323-a-transformacao-pela-graca">A Transformação pela Graça - 03 de Setembro de 2023</Link><br />
-    <Link className="" to="/liturgias/091023-os-filhos-e-herdeiros-em-cristo">Os Filhos e Herdeiros em Cristo - 10 de Setembro de 2023</Link><br />
-    <Link className="" to="/liturgias/091723-desfrutando-da-verdadeira-liberdade-em-cristo">Desfrutando da Verdadeira Liberdade em Cristo - 17 de Setembro de 2023</Link><br />
-    <Link className="" to="/liturgias/092423-uma-igreja-nascida-do-espirito">Uma Igreja Nascida do Espírito - 24 de Setembro de 2023</Link><br />
-    <Link className="" to="/liturgias/100123-compromissados-com-a-liberdade-em-cristo">Compromissados com a Liberdade em Cristo - 01 de Outubro de 2023</Link><br />
-    <Link className="" to="/liturgias/100823-uma-igreja-que-reconhece-sua-miseria-espiritual">Uma Igreja que Reconhece Sua Miséria Espiritual - 08 de Outubro de 2023</Link><br />
-    <Link className="" to="/liturgias/101523-aprendendo-a-esperar-em-deus">Aprendendo a Esperar em Deus - 15 de Outubro de 2023</Link><br />
-    <Link className="" to="/liturgias/102223-uma-igreja-que-anda-no-poder-do-espirito">Uma Igreja que Anda no Poder do Espírito - 22 de Outubro de 2023</Link> <a className="" href="/liturgias/102223-uma-igreja-que-anda-no-poder-do-espirito/download">Download</a><br />
-    <Link className="" to="/liturgias/102923-um-povo-que-se-ajuda-mutuamente">Um Povo que se Ajuda Mutuamente - 29 de Outubro de 2023</Link> <a className="" href="/liturgias/102923-um-povo-que-se-ajuda-mutuamente/download">Download</a><br />
-    <Link className="" to="/liturgias/110523-uma-igreja-que-nao-se-cansa-de-fazer-o-bem">Uma Igreja Que Não Se Cansa de Fazer o Bem - 05 de Novembro de 2023</Link> <a className="" href="/liturgias/110523-uma-igreja-que-nao-se-cansa-de-fazer-o-bem/download">Download</a><br />
-    <Link className="" to="/liturgias/111223-uma-igreja-centrada-na-cruz">Uma Igreja Centrada na Cruz - 12 de Novembro de 2023</Link> <a className="" href="/liturgias/111223-uma-igreja-centrada-na-cruz/download">Download</a><br />
-    <Link className="" to="/liturgias/111923-uma-igreja-cheia-de-esperanca">Uma Igreja Cheia de Esperança - 19 de Novembro de 2023</Link> <a className="" href="/liturgias/111923-uma-igreja-cheia-de-esperanca/download">Download</a><br />
-    <Link className="" to="/liturgias/120323-jesus-a-luz-do-mundo">Jesus, A Luz do Mundo - 03 de Dezembro de 2023</Link> <a className="" href="/liturgias/120323-jesus-a-luz-do-mundo/download">Download</a><br />
-    <Link className="" to="/liturgias/121023-a-encarnacao-de-jesus-cristo">A Encarnação de Jesus Cristo - 10 de Dezembro de 2023</Link> <a className="" href="/liturgias/121023-a-encarnacao-de-jesus-cristo/download">Download</a><br />
-    <Link className="" to="/liturgias/121723-a-superioridade-de-jesus-cristo">A Superioridade de Jesus Cristo - 17 de Dezembro de 2023</Link> <a className="" href="/liturgias/121723-a-superioridade-de-jesus-cristo/download">Download</a><br />
-    <Link className="" to="/liturgias/122423-uma-igreja-que-entende-o-verdadeiro-significado-do-natal">Uma Igreja Que Entende O Verdadeiro Significado Do Natal - 24 de Dezembro de 2023</Link> <a className="" href="/liturgias/122423-uma-igreja-que-entende-o-verdadeiro-significado-do-natal/download">Download</a><br />
+    <Link className="" to="/liturgias/010724-o-cordeiro-de-deus">O Cordeiro de Deus - 07 de Janeiro de 2024</Link> <a className="" href="/liturgias/010724-o-cordeiro-de-deus/download">Download</a><br />
   </div>)
 }
---
+++ liturgias/010724-o-cordeiro-de-deus.md
@@ -0,0 +1,34 @@
+18h, 7 de janeiro de 2024
+Igreja Presbiteriana de Cachoeirinha
+
+O CORDEIRO DE DEUS 
+
+Prelúdio
+
+É A RAZÃO DE NOSSA ADORAÇÃO
+Leitura Bíblica uníssona: Salmo 134
+Oração de adoração
+Hino A Igreja em Adoração, nº 3
+
+OFERECE SALVAÇÃO AOS PECADORES
+Leitura bíblica alternada: Mateus 4.1-11
+Oração silenciosa
+Oração de contrição
+Hino Pureza no Sangue de Cristo, nº 269
+
+SIMBOLIZA PUREZA E SACRIFÍCIO
+Leitura bíblica: 1 Pedro 1.18-19
+Consagração dos dízimos e ofertas
+Hino Linda Melodia, nº 104
+Oração de gratidão
+
+É A ESPERANÇA DA HUMANIDADE
+Oração pelas crianças
+Pregação: João 1.29-34
+
+É O CAMINHO PARA A RECONCILIAÇÃO COM DEUS
+Hino A Mensagem Real, nº 288
+Oração final
+Benção apostólica
+
+Benção antiga
