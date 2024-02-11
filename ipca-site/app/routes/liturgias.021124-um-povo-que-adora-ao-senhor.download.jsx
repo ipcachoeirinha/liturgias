ad14f7494b04744cd107b8bf17c180499344d49a
vv-z84 <43834700+vv-z84@users.noreply.github.com>
2024-02-11 10:35:01 -0300

    Update download page for Feb 11 24

--- ipca-site/app/routes/liturgias.021124-um-povo-que-adora-ao-senhor.download.jsx
+++ ipca-site/app/routes/liturgias.021124-um-povo-que-adora-ao-senhor.download.jsx
@@ -1,21 +1,21 @@
 import defaultConfig from '../config/revealjs-default.json'
 import { downloadSlideRemixLoader } from '../lib.server'
 
-const PAGE_TITLE = "Um Povo Que Segue Jesus Cristo - 14 de Janeiro de 2024"
+const PAGE_TITLE = "Um Povo Que Adora Ao Senhor - 11 de Fevereiro de 2024"
 const CDN_ASSETS_LIST = [
   { id: "ipcachoeirinha/cruz-wallpaper-light", extension: "jpg", resourceType: "image" },
   { id: "ipcachoeirinha/santa-ceia-bg", extension: "png", resourceType: "image" },
-  { id: "ipcachoeirinha/hino-1", extension: "mp4", resourceType: "video" },
-  { id: "ipcachoeirinha/hino-78", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/hino-4", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/hino-110-a", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/hino-115", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/hino-135", extension: "mp4", resourceType: "video" },
   { id: "ipcachoeirinha/hino-340", extension: "mp4", resourceType: "video" },
-  { id: "ipcachoeirinha/hino-58", extension: "mp4", resourceType: "video" },
-  { id: "ipcachoeirinha/hino-109", extension: "mp4", resourceType: "video" },
   { id: "ipcachoeirinha/hino-001-instrumental", extension: "mp3", resourceType: "video" },
   { id: "ipcachoeirinha/uma-bencao-antiga-letra", extension: "mp4", resourceType: "video" },
-  { id: "ipcachoeirinha/what-a-beautiful-name-hillsong-cover-daniel-jang", extension: "mp3", resourceType: "video" },
+  { id: "ipcachoeirinha/oracao-silenciosa-hino-208", extension: "mp3", resourceType: "video" },
 ]
 
-const ZIP_FILENAME = "011424-um-povo-que-segue-jesus-cristo"
+const ZIP_FILENAME = "021124-um-povo-que-adora-ao-senhor"
 
 export async function loader(remixLoaderContext) {
   return downloadSlideRemixLoader(remixLoaderContext, ZIP_FILENAME, CDN_ASSETS_LIST, PAGE_TITLE, {...defaultConfig, width: 1600 })
