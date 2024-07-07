ee4080e03836f99fe40649cb86e3e791c20cc9eb
vv-z84 <43834700+vv-z84@users.noreply.github.com>
2024-07-07 12:01:58 -0300

    Adiciona liturgia do dia 7 de julho

--- ipca-site/app/routes/liturgias._index.jsx
+++ ipca-site/app/routes/liturgias._index.jsx
@@ -20,5 +20,6 @@ export default function Liturgias() {
     <Link className="" to="/liturgias/042824-o-amor-de-deus">O Amor de Deus - 28 de Abril de 2024</Link> <a className="" href="/liturgias/042824-o-amor-de-deus/download">Download</a><br />
     <Link className="" to="/liturgias/050524-cultivando-a-humildade">Cultivando a Humildade - 05 de Maio de 2024</Link> <a className="" href="/liturgias/050524-cultivando-a-humildade/download">Download</a><br />
     <Link className="" to="/liturgias/063024-quando-a-igreja-confia-no-senhor">Quando a igreja confia no Senhor - 30 de Junho de 2024</Link> <a className="" href="/liturgias/063024-quando-a-igreja-confia-no-senhor/download">Download</a><br />
+    <Link className="" to="/liturgias/070724-uma-igreja-encorajada-no-senhor">Uma igreja encorajada no Senhor - 07 de Julho de 2024</Link> <a className="" href="/liturgias/070724-uma-igreja-encorajada-no-senhor/download">Download</a><br />
   </div>)
 }
---
+++ liturgias/070724-uma-igreja-encorajada-no-senhor.md
@@ -0,0 +1,34 @@
+18h, 7 de julho de 2024 - IPCachoeirinha
+
+UMA IGREJA ENCORAJADA NO SENHOR
+
+Prelúdio
+
+DESCANSA NAS TERNAS MISERICÓRDIAS DE DEUS
+•	Leitura bíblica: Salmo 136
+•	Oração de adoração
+•	Hino "Trindade Santíssima" n° 11
+
+CONFIA NA FIDELIDADE DE DEUS
+•	Leitura bíblica: 2ª Timóteo 2.11-13
+•	Oração Silenciosa (interlúdio musical)
+•	Oração de confissão
+•	Hino O Deus Fiel, nº 32
+
+SE APEGA À CERTEZA DA PRESENÇA DE DEUS
+•	Leitura bíblica uníssona: Salmo 23
+•	Oração pastoral
+•	Cânticos congregacionais
+•	Consagração de Dízimos e Ofertas
+
+DESFRUTA DA OBRA DE CRISTO
+•	Oração pelas crianças
+•	Pregação: João 16.23-33 (Rev. Arnaldo Matias)
+
+AGUARDA A INTERVENÇÃO DE DEUS
+•	Hino "Grata Confiança" n° 169
+•	Oração Final
+•	Benção Apostólica
+•	Benção Antiga
+
+Poslúdio
