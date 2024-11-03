8c4e0f57b45dc07ef816b0d6f746ed191641240d
vv-z84 <43834700+vv-z84@users.noreply.github.com>
2024-11-03 10:47:44 -0300

    Fix

--- ipca-site/app/routes/liturgias.102724-a-autoridade-das-escrituras.download.jsx
+++ ipca-site/app/routes/liturgias.102724-a-autoridade-das-escrituras.download.jsx
@@ -1,18 +1,17 @@
 import defaultConfig from '../config/revealjs-default.json'
 import { downloadSlideRemixLoader } from '../lib.server'
 
-const PAGE_TITLE = "Santificados pela Palavra de Deus - 26 de Outubro de 2024"
+const PAGE_TITLE = "A Autoridade das Escrituras - 27 de Outubro de 2024"
 const CDN_ASSETS_LIST = [
   { id: "ipcachoeirinha/cruz-wallpaper-light", extension: "jpg", resourceType: "image" },
-  { id: "ipcachoeirinha/hino-11", extension: "mp4", resourceType: "video" },
-  { id: "ipcachoeirinha/hino-67", extension: "mp4", resourceType: "video" },
-  { id: "ipcachoeirinha/hino-145", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/santa-ceia-bg", extension: "png", resourceType: "image" },
+  { id: "ipcachoeirinha/hino-86", extension: "mp4", resourceType: "video" },
   { id: "ipcachoeirinha/hino-001-instrumental", extension: "mp3", resourceType: "video" },
   { id: "ipcachoeirinha/uma-bencao-antiga-letra", extension: "mp4", resourceType: "video" },
   { id: "ipcachoeirinha/oracao-silenciosa-hino-208", extension: "mp3", resourceType: "video" }
 ]
 
-const ZIP_FILENAME = "102624-santificados-pela-palavra-de-deus"
+const ZIP_FILENAME = "102724-a-autoridade-das-escrituras"
 
 export async function loader(remixLoaderContext) {
   return downloadSlideRemixLoader(remixLoaderContext, ZIP_FILENAME, CDN_ASSETS_LIST, PAGE_TITLE, {...defaultConfig, width: 1600 })
