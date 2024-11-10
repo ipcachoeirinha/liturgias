2bac0028f13441efc723af2c59ee0cd5e3154763
vv-z84 <43834700+vv-z84@users.noreply.github.com>
2024-11-10 17:15:54 -0300

    Fix

--- ipca-site/app/routes/liturgias.111024-progredindo-espiritualmente.download.jsx
+++ ipca-site/app/routes/liturgias.111024-progredindo-espiritualmente.download.jsx
@@ -1,21 +1,18 @@
 import defaultConfig from '../config/revealjs-default.json'
 import { downloadSlideRemixLoader } from '../lib.server'
 
-const PAGE_TITLE = "Vivendo o perdão bíblico - 03 de Novembro de 2024"
+const PAGE_TITLE = "Progredindo Espiritualmente - 10 de Novembro de 2024"
 const CDN_ASSETS_LIST = [
   { id: "ipcachoeirinha/cruz-wallpaper-light", extension: "jpg", resourceType: "image" },
-  { id: "ipcachoeirinha/querido-visitante", extension: "png", resourceType: "image" },
-  { id: "ipcachoeirinha/edital-convocacao-100424", extension: "png", resourceType: "image" },
-  { id: "ipcachoeirinha/hino-32", extension: "mp4", resourceType: "video" },
-  { id: "ipcachoeirinha/hino-38", extension: "mp4", resourceType: "video" },
-  { id: "ipcachoeirinha/hino-62", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/hino-121", extension: "mp4", resourceType: "video" },
   { id: "ipcachoeirinha/hino-59", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/hino-147", extension: "mp4", resourceType: "video" },
   { id: "ipcachoeirinha/hino-001-instrumental", extension: "mp3", resourceType: "video" },
   { id: "ipcachoeirinha/uma-bencao-antiga-letra", extension: "mp4", resourceType: "video" },
   { id: "ipcachoeirinha/oracao-silenciosa-hino-208", extension: "mp3", resourceType: "video" }
 ]
 
-const ZIP_FILENAME = "110324-vivendo-o-perdao-biblico"
+const ZIP_FILENAME = "111024-progredindo-espiritualmente"
 
 export async function loader(remixLoaderContext) {
   return downloadSlideRemixLoader(remixLoaderContext, ZIP_FILENAME, CDN_ASSETS_LIST, PAGE_TITLE, {...defaultConfig, width: 1600 })
