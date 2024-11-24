6714d64aaaaccf8a91524ccd56b2f7794ccf9c2f
vv-z84 <43834700+vv-z84@users.noreply.github.com>
2024-11-24 15:45:41 -0300

    Adiciona liturgia de 24/11/24

---
+++ ipca-site/app/routes/liturgias.112424-gratos-por-pertencer-a-igreja-de-cristo.download.jsx
@@ -0,0 +1,21 @@
+import defaultConfig from '../config/revealjs-default.json'
+import { downloadSlideRemixLoader } from '../lib.server'
+
+const PAGE_TITLE = "Gratos por pertencer à igreja de Cristo - 24 de Novembro de 2024"
+const CDN_ASSETS_LIST = [
+  { id: "ipcachoeirinha/cruz-wallpaper-light", extension: "jpg", resourceType: "image" },
+  { id: "ipcachoeirinha/querido-visitante", extension: "png", resourceType: "image" },
+  { id: "ipcachoeirinha/hino-3", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/hino-78", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/hino-59", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/hino-172", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/hino-001-instrumental", extension: "mp3", resourceType: "video" },
+  { id: "ipcachoeirinha/uma-bencao-antiga-letra", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/oracao-silenciosa-hino-208", extension: "mp3", resourceType: "video" }
+]
+
+const ZIP_FILENAME = "112424-gratos-por-pertencer-a-igreja-de-cristo"
+
+export async function loader(remixLoaderContext) {
+  return downloadSlideRemixLoader(remixLoaderContext, ZIP_FILENAME, CDN_ASSETS_LIST, PAGE_TITLE, {...defaultConfig, width: 1600 })
+}
---
+++ liturgias/112424-gratos-por-pertencer-a-igreja-de-cristo.md
@@ -0,0 +1,34 @@
+18h, 24 de novembro de 2024
+Igreja Presbiteriana de Cachoeirinha
+
+GRATOS POR PERTENCER À IGREJA DE CRISTO
+ 
+Prelúdio 
+
+NOS PROSTRAMOS EM ADORAÇÃO AO DEUS VIVO
+Leitura bíblica: 2º Timóteo 3.14-16
+Oração de adoração
+Hino A Igreja em Adoração, nº 3
+ 
+CONFESSAMOS NOSSOS PECADOS AO DEUS MISERICORDIOSO
+Leitura bíblica uníssona: 1 João 1.8-9
+Oração silenciosa
+Oração de confissão
+Hino Perfeita expiação, nº 78
+ 
+VALORIZAMOS A COMUNHÃO VIVIDA EM AMBIENTE PACTUAL
+Leitura bíblica alternada: Atos 2.42-47
+Leitura bíblica alternada: Atos 5.32-37
+Oração de gratidão
+Consagração dos dízimos e ofertas
+Cântico Alto Preço ou Hino Gratidão, nº 59  
+ 
+PRIORIZAMOS OUVIR A PREGAÇÃO DA PALAVRA DE DEUS
+Oração pelas crianças
+Pregação: Colossenses 1.1-8 - Rev Arnaldo Matias
+ 
+DESFRUTAMOS DE CHUVAS DE BÊNÇÃOS
+Hino Chuvas de Bênçãos, nº 172
+Oração final
+Benção apostólica
+Tríplice amém
