53591770dcb02ecf7b244c5afd7a4e33ffda982d
vv-z84 <43834700+vv-z84@users.noreply.github.com>
2024-12-22 17:18:44 -0300

    Fix download 22/12

--- ipca-site/app/routes/liturgias.122224-uma-alma-que-engrandece-ao-senhor.download.jsx
+++ ipca-site/app/routes/liturgias.122224-uma-alma-que-engrandece-ao-senhor.download.jsx
@@ -1,14 +1,11 @@
 import defaultConfig from '../config/revealjs-default.json'
 import { downloadSlideRemixLoader } from '../lib.server'
 
-const PAGE_TITLE = "A Alegria do Natal - 15 de Dezembro de 2024"
+const PAGE_TITLE = "Uma alma que engrandece ao Senhor - 22 de Dezembro de 2024"
 const CDN_ASSETS_LIST = [
   { id: "ipcachoeirinha/cruz-wallpaper-light", extension: "jpg", resourceType: "image" },
-  { id: "ipcachoeirinha/querido-visitante", extension: "png", resourceType: "image" },
-  { id: "ipcachoeirinha/psola-isaias-53-playback1", extension: "mp4", resourceType: "video" },
-  { id: "ipcachoeirinha/vporcristo-doce-nome-playback1", extension: "mp4", resourceType: "video" },
-  { id: "ipcachoeirinha/hino-239", extension: "mp4", resourceType: "video" },
-  { id: "ipcachoeirinha/hino-230", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/hino-240", extension: "mp4", resourceType: "video" },
+  { id: "ipcachoeirinha/hino-231", extension: "mp4", resourceType: "video" },
   { id: "ipcachoeirinha/hino-001-instrumental", extension: "mp3", resourceType: "video" },
   { id: "ipcachoeirinha/uma-bencao-antiga-letra", extension: "mp4", resourceType: "video" },
   { id: "ipcachoeirinha/oracao-silenciosa-hino-208", extension: "mp3", resourceType: "video" }
