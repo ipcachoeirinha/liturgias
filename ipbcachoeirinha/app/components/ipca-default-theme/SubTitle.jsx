3a944d824cdba428f5b61f9d6a327a01a85df40d
vv-z84 <43834700+vv-z84@users.noreply.github.com>
2025-06-01 04:12:27 -0300

    Melhoria visual

--- ipbcachoeirinha/app/components/ipca-default-theme/SubTitle.jsx
+++ ipbcachoeirinha/app/components/ipca-default-theme/SubTitle.jsx
@@ -1,3 +1,3 @@
 export function SubTitle({ children }) {
-    return <h2 className="text-green-900 font-bold text-6xl mb-16 tracking-wide">{children}</h2>
+    return <h2 className="text-green-900 font-bold text-6xl mb-16 tracking-wide leading-[1.2]">{children}</h2>
 }
