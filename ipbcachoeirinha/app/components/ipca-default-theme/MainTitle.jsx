2f8687d57edc05c8bfeb939c37b6453f672d8565
vv-z84 <43834700+vv-z84@users.noreply.github.com>
2026-01-25 01:11:21 -0300

    Ajuste visual

--- ipbcachoeirinha/app/components/ipca-default-theme/MainTitle.jsx
+++ ipbcachoeirinha/app/components/ipca-default-theme/MainTitle.jsx
@@ -1,3 +1,3 @@
 export function MainTitle({ children }) {
-    return <h1 className="text-yellow-700 font-bold text-7xl tracking-wider mb-8 leading-[1.2]">{children}</h1>
+    return <h1 className="text-yellow-700 font-bold text-6xl tracking-wider mb-8 leading-[1.2]">{children}</h1>
 }
